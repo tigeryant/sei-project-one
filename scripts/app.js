ac280a27540c26db61629d5f0da5388a90d78822
John Davies <jd.other@hotmail.com>
2021-11-15 12:16:35 +0000

    first tasks

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,19 @@
 class Data {
+  constructor() {
 
+  }
 }
 
 class Pacman {
+  constructor() {
 
+  }
 }
 
 class Ghost {
+  constructor() {
 
+  }
 }
 
 function main() {
@@ -17,7 +23,9 @@ function main() {
 }
 
 function init() {
-
+  /*
+  Instantiate the pacman object, ghost objects, and the data object by calling their constructors. Add the ghost objects to a ghosts array. In the data constructor, declare game data (constants, variables, arrays, 2d arrays, nodelists). Also within the data constructor, declare and initialise the corresponding dom elements.
+  */
 }
 
 function runGame() {
