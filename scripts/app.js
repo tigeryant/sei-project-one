b3cac76a6f1ba5d23c2a307d739587a1ae471eb0
John Davies <jd.other@hotmail.com>
2021-11-15 00:57:48 +0000

    basic structure

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+class Data {
+
+}
+
+class Pacman {
+
+}
+
+class Ghost {
+
+}
+
+function main() {
+  init()
+  runGame()
+  handleFatalCollision()
+}
+
+function init() {
+
+}
+
+function runGame() {
+
+}
+
+function handleFatalCollision() {
+
+}
+
+main()
