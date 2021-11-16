9aa7e677bd5c96b41a63fe6697ea236902f3712f
John Davies <jd.other@hotmail.com>
2021-11-16 14:44:27 +0000

    more init of arrays

--- scripts/app.js
+++ scripts/app.js
@@ -12,6 +12,9 @@ class Data {
     this.ghost4StartX = 16
     this.ghostStartY = 14
 
+    this.smallScore = 10
+    this.bigScore = 50
+
     this.pacmanTimer = null
     // now deprecated:
     this.ghost1CycleTimer = null
@@ -24,25 +27,25 @@ class Data {
     this.ghost4MoveTimer = null
 
 
-
     this.livesLeft = 3
     this.score = 0
     this.fatalCollision = false
     this.ghosts = []
 
-    this.pacmansPos = []
+    this.pacmansPos = [] // not needed
     this.allGhostsPos = []
 
     // declare cells, rows, walls(array of objects/2d arrays)
-    this.cells = []
-    this.walls = []
+    // this.cells = []
+    // this.walls = []
 
     // declare position arrays
     this.tunnelPositions = [ // not including portals or ghost house
       [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [15, 1], [16, 1], [17, 1], [18, 1], [19, 1], [20, 1], [21, 1], [22, 1], [23, 1], [24, 1], [25, 1], [26, 1], [1, 2], [6, 2], [12, 2], [15, 2], [21, 2], [26, 2], [1, 3], [6, 3], [12, 3], [15, 3], [21, 3], [26, 3], [1, 4], [6, 4], [12, 4], [15, 4], [21, 4], [26, 4], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5], [12, 5], [13, 5], [14, 5], [15, 5], [16, 5], [17, 5], [18, 5], [19, 5], [20, 5], [21, 5], [22, 5], [23, 5], [24, 5], [25, 5], [26, 5], [1, 6], [6, 6], [9, 6], [18, 6], [21, 6], [26, 6], [1, 7], [6, 7], [9, 7], [18, 7], [21, 7], [26, 7], [1, 8], [2, 8], [3, 8], [4, 8], [5, 8], [6, 8], [9, 8], [10, 8], [11, 8], [12, 8], [15, 8], [16, 8], [17, 8], [18, 8], [21, 8], [22, 8], [23, 8], [24, 8], [25, 8], [26, 8], [6, 9], [12, 9], [15, 9], [21, 9], [6, 10], [12, 10], [15, 10], [21, 10], [6, 11], [9, 11], [10, 11], [11, 11], [12, 11], [13, 11], [14, 11], [15, 11], [16, 11], [17, 11], [18, 11], [21, 11], [6, 12], [9, 12], [18, 12], [21, 12], [6, 13], [9, 13], [18, 13], [21, 13], [1, 14], [2, 14], [3, 14], [4, 14], [5, 14], [6, 14], [7, 14], [8, 14], [9, 14], [18, 14], [19, 14], [20, 14], [21, 14], [22, 14], [23, 14], [24, 14], [25, 14], [26, 14], [6, 15], [9, 15], [18, 15], [21, 15], [6, 16], [9, 16], [18, 16], [21, 16], [6, 17], [9, 17], [10, 17], [11, 17], [12, 17], [13, 17], [14, 17], [15, 17], [16, 17], [17, 17], [18, 17], [21, 17], [6, 18], [9, 18], [18, 18], [21, 18], [6, 19], [9, 19], [18, 19], [21, 19], [1, 20], [2, 20], [3, 20], [4, 20], [5, 20], [6, 20], [7, 20], [8, 20], [9, 20], [10, 20], [11, 20], [12, 20], [15, 20], [16, 20], [17, 20], [18, 20], [19, 20], [20, 20], [21, 20], [22, 20], [23, 20], [24, 20], [25, 20], [26, 20], [1, 21], [6, 21], [12, 21], [15, 21], [21, 21], [26, 21], [1, 22], [6, 22], [12, 22], [15, 22], [21, 22], [26, 22], [1, 23], [2, 23], [3, 23], [6, 23], [7, 23], [8, 23], [9, 23], [10, 23], [11, 23], [12, 23], [13, 23], [14, 23], [15, 23], [16, 23], [17, 23], [18, 23], [19, 23], [20, 23], [21, 23], [24, 23], [25, 23], [26, 23], [3, 24], [6, 24], [9, 24], [18, 24], [21, 24], [24, 24], [3, 25], [6, 25], [9, 25], [18, 25], [21, 25], [24, 25], [1, 26], [2, 26], [3, 26], [4, 26], [5, 26], [6, 26], [9, 26], [10, 26], [11, 26], [12, 26], [15, 26], [16, 26], [17, 26], [18, 26], [21, 26], [22, 26], [23, 26], [24, 26], [25, 26], [26, 26], [1, 27], [12, 27], [15, 27], [26, 27], [1, 28], [12, 28], [15, 28], [26, 28], [1, 29], [2, 29], [3, 29], [4, 29], [5, 29], [6, 29], [7, 29], [8, 29], [9, 29], [10, 29], [11, 29], [12, 29], [13, 29], [14, 29], [15, 29], [16, 29], [17, 29], [18, 29], [19, 29], [20, 29], [21, 29], [22, 29], [23, 29], [24, 29], [25, 29], [26, 29]
     ]
 
-    // TODO define the wallsPosition array based on all cells - tunnelPositions - portals - ghostHouse
+
+    // TODO see phone notes
 
     // Declare arrays that store lists of ordered pairs (x and y positions) representing the positions of portals, smallFood, bigFood
     this.portals = [
@@ -50,15 +53,81 @@ class Data {
       [27, 14]
     ]
 
-    // this.smallFood
-    // TODO
-    // this array is populated with positions of every position on the grid, with the exceptions of the walls, portal pathways, the ghost house and the ring around the ghost house
+    this.smallFood = [
+      [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [15, 1], [16, 1], [17, 1], [18, 1], [19, 1], [20, 1], [21, 1], [22, 1], [23, 1], [24, 1], [25, 1], [26, 1], [1, 2], [6, 2], [12, 2], [15, 2], [21, 2], [26, 2], [6, 3], [12, 3], [15, 3], [21, 3], [1, 4], [6, 4], [12, 4], [15, 4], [21, 4], [26, 4], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5], [12, 5], [13, 5], [14, 5], [15, 5], [16, 5], [17, 5], [18, 5], [19, 5], [20, 5], [21, 5], [22, 5], [23, 5], [24, 5], [25, 5], [26, 5], [1, 6], [6, 6], [9, 6], [18, 6], [21, 6], [26, 6], [1, 7], [6, 7], [9, 7], [18, 7], [21, 7], [26, 7], [1, 8], [2, 8], [3, 8], [4, 8], [5, 8], [6, 8], [9, 8], [10, 8], [11, 8], [12, 8], [15, 8], [16, 8], [17, 8], [18, 8], [21, 8], [22, 8], [23, 8], [24, 8], [25, 8], [26, 8], [6, 9], [21, 9], [6, 10], [21, 10], [6, 11], [21, 11], [6, 12], [21, 12], [6, 13], [21, 13], [6, 14], [21, 14], [6, 15], [21, 15], [6, 16], [21, 16], [6, 17], [21, 17], [6, 18], [21, 18], [6, 19], [21, 19], [1, 20], [2, 20], [3, 20], [4, 20], [5, 20], [6, 20], [7, 20], [8, 20], [9, 20], [10, 20], [11, 20], [12, 20], [15, 20], [16, 20], [17, 20], [18, 20], [19, 20], [20, 20], [21, 20], [22, 20], [23, 20], [24, 20], [25, 20], [26, 20], [1, 21], [6, 21], [12, 21], [15, 21], [21, 21], [26, 21], [1, 22], [6, 22], [12, 22], [15, 22], [21, 22], [26, 22], [2, 23], [3, 23], [6, 23], [7, 23], [8, 23], [9, 23], [10, 23], [11, 23], [12, 23], [13, 23], [14, 23], [15, 23], [16, 23], [17, 23], [18, 23], [19, 23], [20, 23], [21, 23], [24, 23], [25, 23], [3, 24], [6, 24], [9, 24], [18, 24], [21, 24], [24, 24], [3, 25], [6, 25], [9, 25], [18, 25], [21, 25], [24, 25], [1, 26], [2, 26], [3, 26], [4, 26], [5, 26], [6, 26], [9, 26], [10, 26], [11, 26], [12, 26], [15, 26], [16, 26], [17, 26], [18, 26], [21, 26], [22, 26], [23, 26], [24, 26], [25, 26], [26, 26], [1, 27], [12, 27], [15, 27], [26, 27], [1, 28], [12, 28], [15, 28], [26, 28], [1, 29], [2, 29], [3, 29], [4, 29], [5, 29], [6, 29], [7, 29], [8, 29], [9, 29], [10, 29], [11, 29], [12, 29], [13, 29], [14, 29], [15, 29], [16, 29], [17, 29], [18, 29], [19, 29], [20, 29], [21, 29], [22, 29], [23, 29], [24, 29], [25, 29], [26, 29]
+    ]
 
-    // this.bigFood
     this.bigFood = [
       [1, 3], [26, 3], [1, 23], [26, 23]
     ]
 
+    this.ghostHouse = [
+      [13, 12], [14, 12], [11, 13], [12, 13], [13, 13], [14, 13], [15, 13], [16, 13], [11, 14], [12, 14], [13, 14], [14, 14], [15, 14], [16, 14], [11, 15], [12, 15], [13, 15], [14, 15], [15, 15], [16, 15]
+    ]
+
+    this.notWalls = this.tunnelPositions.concat(this.ghostHouse).concat(this.portals)
+
+    // adds a class of 'big-food' or 'small-food' to relevant DOM cells in the comCellsArray
+    domCellsArray.forEach(domCell => {
+      // add a wall class to each non-wall domcell
+      this.notWalls.forEach(position => {
+        if (domCell.getAttribute('x') !== position[0] && domCell.getAttribute('y') !== position[1]) {
+          domCell.classList.add('wall')
+        }
+      })
+
+      // adds the smallfood class to all smallfood domcells
+      this.smallFood.forEach(position => {
+        if (domCell.getAttribute('x') === position[0] && domCell.getAttribute('y') === position[1]) {
+          domCell.classList.add('small-food')
+        }
+      })
+
+      // add the bigfood class to all bigfood domcells
+      this.bigFood.forEach(position => {
+        if (domCell.getAttribute('x') === position[0] && domCell.getAttribute('y') === position[1]) {
+          domCell.classList.add('big-food')
+        }
+      })
+
+
+    })
+
+    // populates walls (object) array (make space for portals)
+    this.cells.forEach(row => {
+      row.forEach(cell => {
+        this.notWalls.forEach(position => {
+          if (cell.xPos !== position[0] && cell.yPos !== position[1]) {
+            cell.isWall = true
+            this.walls.push(cell)
+          }
+        })
+      })
+    })
+
+
+    // ! ignore:
+
+    // TODO define the wallsPosition array based on all cells - tunnelPositions - portals - ghostHouse. Use the testbed to test if the above values are correct
+
+    // wallPositions is a filter of cells such that if that element is not included in tunnelpositions, ghosthouse or portals, it is returned as part of wallPositions
+    // TODO define cellPositions first, use cell.xPos, cell.yPos
+    // first concatenate ghostHouse, portals, and tunnelpositions
+
+
+    // const wallPositions = []
+
+    // define cellPositions by taking the cells array and mapping it to an array of positions
+    // TODO
+
+    // cellPositions.forEach(cell => {
+
+    //   notWalls.forEach(element => {
+    //     if (cell[0] !== element[0] && cell[1] !== element[1]) {
+    //       wallPositions.push(cell)
+    //     }
+    //   })
+    // })
   }
 
   initDOM() {
@@ -219,7 +288,8 @@ const grid = document.querySelector('.grid-container')
 grid.style.height = `${data.height * data.cellHeight}px`
 grid.style.width = `${data.width * data.cellWidth}px`
 
-// declaring and populating cells[], adding DOMcells to the DOM
+// declaring and populating cells[] (an array of objects), adding DOMcells to the DOM
+// cell positions can thus be accessed by cell.xPos and cell.yPos (no need for a positional array)
 for (let y = 0; y < data.height; y++) {
   const row = []
   for (let x = 0; x < data.width; x++) {
@@ -243,15 +313,7 @@ for (let y = 0; y < data.height; y++) {
   data.cells.push(row)
 }
 
-// populates walls array (extend this later, make space for portals)
-data.cells.forEach(row => {
-  row.forEach(cell => {
-    if (cell.xPos === 0 || cell.yPos === 0 || cell.xPos === (data.width - 1) || cell.yPos === (data.height - 1)) {
-      cell.isWall = true
-      data.walls.push(cell)
-    }
-  })
-})
+// walls array population was moved from here. If it breaks, put it back
 
 // create a node list of all domCells
 const domCellsNodeList = document.querySelectorAll('.grid-item')
@@ -307,6 +369,24 @@ function runGame() {
     }
 
     // check if pacman's position matches a position in the smallFood position array. If it does, remove that pair from the smallFood array, change that DOM cell by removing its smallFood class and increment data.score by a const smallScore
+
+    // if pacman eats smallFood, remove that element from smallFood positional array, increase score
+    data.smallFood.forEach(position => {
+      if (position[0] === pacman.xPos && position[1] === pacman.yPos) {
+        const index = data.smallFood.indexOf(position)
+        data.smallFood.splice(index, 1)
+
+        data.score += data.smallScore
+      }
+    })
+
+    // if pacman steps on smallfood, remove that class from the dom cell
+    domCellsArray.forEach(domCell => {
+      if (domCell.getAttribute('x') === pacman.xPos && domCell.getAttribute('y') === pacman.yPos) {
+        domCell.classList.remove('small-food')
+      }
+    })
+
     // check if pacman's position matches a position in the bigFood position array. If it does, remove that element of the array, increment data.score by another predefined constant, call the ghosthandler which changes each ghostmode to frightened until either: a timeout expires (in which case the normal mode cycle interval is re-engaged) or there is a match between pacman and the ghosts position, in which case their 'back to base' mode is activated, where they wait for a timeout before returning to the normal mode/cycle interval.
     // if pacman's position matches that of a ghost and their mode is anything except frightened or 'back to base', fatalCollision is set to true
   }
